360da77e837cd906759216958fd62aff460fd67a
Guido Juliano Mazzuchini Ubaldi <91292914+guidojuliano@users.noreply.github.com>
2025-09-17 19:30:17 -0300

    Delete gestion-stock-restaurantes directory

--- gestion-stock-restaurantes/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_gestion-stock-restaurantes_FILE C:/Users/julia/Documents/UTN FRGP/Programacion 2/gestion-stock-restaurantes/gestion-stock-restaurantes/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/gestion-stock-restaurantes.exe)
-set(__QT_DEPLOY_TARGET_gestion-stock-restaurantes_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_gestion-stock-restaurantes_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
